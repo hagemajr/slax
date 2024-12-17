eef224ea5dc5e97a3256ffcf15157c38ff5c6c8e
John Hageman <john.r.hageman@gmail.com>
2024-12-16 20:33:52 -0600

    Added timezones

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -80,7 +80,7 @@ defmodule SlaxWeb.ChatRoomLive do
         </ul>
       </div>
       <div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
-        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
+        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} timezone={@timezone} />
       </div>
       <div class="h-12 bg-white px-4 pb-4">
         <.form
@@ -130,6 +130,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
+  attr :timezone, :string, required: true
 
   defp message(assigns) do
     ~H"""
@@ -140,7 +141,9 @@ defmodule SlaxWeb.ChatRoomLive do
           <.link class="text-sm font-semibold hover:underline">
             <span><%= username(@message.user) %></span>
           </.link>
-          <span class="m1-1 text-xs text-gray-500>"><%= message_timestamp(@message) %></span>
+          <span :if={@timezone} class="ml-1 text-xs text-gray-500">
+            <%= message_timestamp(@message, @timezone) %>
+          </span>
           <p class="text-sm"><%= @message.body %></p>
         </div>
       </div>
@@ -155,8 +158,9 @@ defmodule SlaxWeb.ChatRoomLive do
     |> String.capitalize()
   end
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
     message.inserted_at
+    |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
   end
 
@@ -178,8 +182,8 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
-
-    {:ok, assign(socket, hide_topic?: false, rooms: rooms)}
+    timezone = get_connect_params(socket)["timezone"]
+    {:ok, assign(socket, hide_topic?: false, rooms: rooms, timezone: timezone)}
   end
 
   defp assign_message_form(socket, changeset) do
