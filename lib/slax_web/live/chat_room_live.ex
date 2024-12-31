83eabab28dac314fae94dc17949a05b76a5aec94
John Hageman <john.r.hageman@gmail.com>
2024-12-30 20:08:58 -0600

    Adding pubsub

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -181,6 +181,8 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_params(params, _session, socket) do
+    if socket.assigns[:room], do: Chat.unsubscribe_from_room(socket.assigns.room)
+
     room =
       case Map.fetch(params, "id") do
         {:ok, id} -> Chat.get_room!(id)
@@ -188,6 +190,8 @@ defmodule SlaxWeb.ChatRoomLive do
       end
 
     messages = Chat.list_messages_in_roon(room)
+
+    Chat.subscribe_to_room(room)
 
     {:noreply,
      socket
@@ -217,9 +221,8 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_event("delete-message", %{"id" => id}, socket) do
-    {:ok, message} = Chat.delete_message_by_id(id, socket.assigns.current_user)
-
-    {:noreply, stream_delete(socket, :messages, message)}
+    Chat.delete_message_by_id(id, socket.assigns.current_user)
+    {:noreply, socket}
   end
 
   def handle_event("submit-message", %{"message" => message_params}, socket) do
@@ -228,14 +231,20 @@ defmodule SlaxWeb.ChatRoomLive do
     socket =
       case Chat.create_message(room, message_params, current_user) do
         {:ok, message} ->
-          socket
-          |> stream_insert(:messages, message)
-          |> assign_message_form(Chat.change_message(%Message{}))
+          assign_message_form(Chat.change_message(%Message{}))
 
         {:error, changeset} ->
           assign_message_form(socket, changeset)
       end
 
     {:noreply, socket}
+  end
+
+  def handle_info({:new_message, message}, socket) do
+    {:noreply, stream_insert(socket, :messages, message)}
+  end
+
+  def handle_info({:message_deleted, message}, socket) do
+    {:noreply, stream_delete(socket, :messages, message)}
   end
 end
